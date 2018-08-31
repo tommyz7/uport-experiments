8fe6fd94e2145f0658edc4bbbd6f5f1204973cb1
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2018-08-31 11:36:38 +0200

    set default value

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.4.17;
 contract SimpleStorage {
   uint storedData;
 
+  constructor() public {
+    storedData = 10;
+  }
+
   function set(uint x) public {
     storedData = x;
   }
