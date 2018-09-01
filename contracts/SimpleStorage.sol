1bdfd431ab56e5a6c166db9c84097093bf6d463a
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2018-09-02 00:57:39 +0200

    added event for return value

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.4.17;
 contract SimpleStorage {
   uint storedData;
 
+  event StorageUpdate(uint value);
+
   constructor() public {
     storedData = 10;
   }
 
   function set(uint x) public {
     storedData = x;
+    emit StorageUpdate(x);
   }
 
   function get() public view returns (uint) {
